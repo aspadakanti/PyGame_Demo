3bfd92706a8715ea1e3a3ddda3b42e7a731ed998
Ananth Goyal <ananthgoyal@gmail.com>
2020-06-24 17:52:42 -0700

    basics

--- Test.py
+++ Test.py
@@ -3,6 +3,8 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((400, 300))
 done = False
+is_blue = True
+
 
 while not done:
     for event in pygame.event.get():
@@ -10,3 +12,11 @@ while not done:
             done = True
 
     pygame.display.flip()
+    if is_blue:
+        color = (0, 128, 255)
+    else:
+        color = (255, 100, 0)
+    pygame.draw.rect(screen, color, pygame.Rect(30, 30, 60, 60))
+
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+        is_blue = not is_blue
