6ec9431a9c6d7b5ecc394f15dd49c0249c7bcaa1
Ananth Goyal <ananthgoyal@gmail.com>
2020-06-24 17:47:23 -0700

    ananth2

--- Test.py
+++ Test.py
@@ -1,3 +1,12 @@
 import pygame
-#ameya
-#ananth
+
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+done = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    pygame.display.flip()
